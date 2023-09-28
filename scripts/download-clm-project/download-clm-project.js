2d8252503750501d6c5d86b5db1df2904a1addd7
mcasanueva1 <116030641+mcasanueva1@users.noreply.github.com>
2023-09-28 17:33:54 -0300

    Update download-clm-project.js

--- scripts/download-clm-project/download-clm-project.js
+++ scripts/download-clm-project/download-clm-project.js
@@ -7,28 +7,6 @@ import rateLimit from "axios-rate-limit";
 import { Command } from "commander";
 import pLimit from "p-limit";
 
-//metadata template
-let metadata = {
-  source: "zeplin",
-  project: {
-    id: null,
-    name: null,
-  },
-  screens: {
-    template: {
-      id: null,
-      name: null,
-      assets: {
-        template: {
-
-        },
-        data: []
-      }
-    },
-    data: []
-  },
-}
-
 // Set your dotenv config to the root directory where the .env file lives
 config({ path: "../../.env" });
 
@@ -52,25 +30,42 @@ const getProjectProperties = async (projectId) => {
   return data;
 };
 
-const getProjectScreens = async (projectId) => {
+const getProjectScreens = async (projectId, screenId) => {
   const { data } = await zeplin.screens.getProjectScreens(projectId);
 
-  return data;
+  if (screenId) {
+    return data.filter((screen) => screen.id === screenId);
+  } else {
+    return data;
+  }
 };
 
-const getAssetData = async (screen, projectId, formats) => {
+const getAssetData = async (screen, projectId, formats, densities) => {
   const { id, name } = screen;
   const { data } = await zeplin.screens.getLatestScreenVersion(projectId, id);
-  return data.assets.flatMap(({ displayName, contents }) => {
+
+  return data.assets.flatMap(({ displayName, contents, layerSourceId, layerName }) => {
     // remove any asset that are not in the formats defined in PROJECT_OPTIONS.formats
-    const filteredContents = contents.filter((content) => formats.includes(content.format));
+    const filteredContents = contents.filter((content) => formats.includes(content.format) && densities.includes(content.density.toString()));
     return filteredContents.map(({ url, format, density }) => ({
       screenId: id,
       screenName: name,
+      displayName,
+      layerSourceId,
+      layerName,
       url,
+      format,
+      density,
       filename: `${displayName.replaceAll("/", "-")}-${density}x.${format}`,
     }));
   });
+};
+
+const getLayerData = async (screen, projectId) => {
+  const { id } = screen;
+  const { data } = await zeplin.screens.getLatestScreenVersion(projectId, id);
+
+  return { screenId: id, layers: data.layers };
 };
 
 const downloadAsset = async ({ name, url, filename }, dir, progress) => {
@@ -85,6 +80,54 @@ const downloadAsset = async ({ name, url, filename }, dir, progress) => {
   progress.tick();
 };
 
+// metadata object to be saved to metadata.json
+let metadata = {
+  source: "zeplin",
+  project: {
+    id: null,
+    name: null,
+  },
+  screens: {
+    template: {
+      id: null,
+      name: null,
+      assets: {
+        template: {
+          displayName: null,
+          filename: null,
+          format: null,
+          density: null,
+          layer: {
+            id: null,
+            name: null,
+          },
+        },
+        data: [],
+      },
+      layers: {
+        template: {
+          id: null,
+          sourceId: null,
+          name: null,
+          type: null,
+          rect: {
+            width: null,
+            height: null,
+            x: 0,
+            y: 0,
+            absolute: {
+              x: 0,
+              y: 0,
+            },
+          },
+          layers: [],
+        },
+        data: [],
+      },
+    },
+    data: [],
+  },
+};
 const updateMetadata = {
   projectId: (projectId) => {
     metadata.project.id = projectId;
@@ -102,31 +145,108 @@ const updateMetadata = {
     delete metadata.screens.template;
   },
   assets: (assets) => {
-    
     assets.forEach((asset) => {
       let screenIndex = metadata.screens.data.findIndex((screen) => screen.id === asset.screenId);
-      console.log(screenIndex)
+      if (screenIndex !== -1) {
+        let template = JSON.parse(JSON.stringify(metadata.screens.data[screenIndex].assets.template));
+        template.displayName = asset.displayName;
+        template.filename = asset.filename;
+        template.format = asset.format;
+        template.density = asset.density;
+        template.layer.id = asset.layerSourceId;
+        template.layer.name = asset.layerName;
+        metadata.screens.data[screenIndex].assets.data.push(template);
+      }
     });
-  }
-}
+    metadata.screens.data.forEach((screen) => {
+      delete screen.assets.template;
+    });
+  },
+  layers: (screensLayers) => {
+    screensLayers.forEach((screenLayers) => {
+      let screenIndex = metadata.screens.data.findIndex((screen) => screen.id === screenLayers.screenId);
+      if (screenIndex !== -1) {
+        metadata.screens.data[screenIndex].layers.data = screenLayers.layers;
+      }
+    });
+    // trim layers tree
+    metadata.screens.data.forEach((screen) => {
+      screen.layers.data.forEach((layer) => {
+        layer = updateMetadata.trimLayersFields(layer, screen.layers.template);
+      });
+    });
+    // remove template
+    metadata.screens.data.forEach((screen) => {
+      delete screen.layers.template;
+    });
+    //combine layers and assets
+    metadata.screens.data.forEach((screen) => {
+      screen.assets.data.forEach((asset) => {
+        screen.layers.data.forEach((layer) => {
+          updateMetadata.updateLayerWithAsset(asset, layer)
+        });
+      });
+    });
+    //remove branches without assets
+    metadata.screens.data.forEach((screen) => {
+      screen.layers.data.forEach((layer) => {
+        //trim branches without assets
+      });
+    });
+  },
+  trimLayersFields: (layer, template) => {
+    let layerKeys = Object.keys(layer);
+    let templateKeys = Object.keys(template);
+    let keysToRemove = layerKeys.filter((key) => !templateKeys.includes(key));
+    keysToRemove.forEach((key) => {
+      delete layer[key];
+    });
+    if (layer.layers) {
+      layer.layers = layer.layers.map((layer) => updateMetadata.trimLayersFields(layer, template));
+    }
+    return layer;
+  },
+  updateLayerWithAsset: (asset, layer) => {
+    if (layer.sourceId == asset.layer.id) {
+      layer.asset = asset;
+    }
+    if (layer.layers) {
+      layer.layers.forEach((layer) => updateMetadata.updateLayerWithAsset(asset, layer));
+    }
+  },
+  trimLayersBranches: (layer) => {},
+};
+
+const saveMetadata = (folder, data) => {
+  fs.writeFile(`${folder}/metadata.json`, JSON.stringify(data, null, 2), (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+};
 
 // add command line options
 program
   .requiredOption("-p, --projectId <projectId>", "Project ID")
+  .option("-s, --screenId <screenId>", null)
   .option("-mo, --metadataOnly", "Download metadata only (no assets)", false)
   .option("-d, --directory <dir>", "Output directory", "Output")
   .option("-f, --formats <formats...>", "Formats to download", ["png", "jpg", "webp", "svg", "pdf"])
-  .action(async ({ projectId, metadataOnly, directory, formats }) => {
+  .option("-e, --densities <density...>", "Density to download", ["1", "1.5", "2", "3", "4"])
+  .action(async ({ projectId, screenId, metadataOnly, directory, formats, densities }) => {
     updateMetadata.projectId(projectId);
 
     const { name: projectName } = await getProjectProperties(projectId);
     updateMetadata.projectName(projectName);
 
-    const projectScreens = await getProjectScreens(projectId);
+    const projectScreens = await getProjectScreens(projectId, screenId);
     updateMetadata.screens(projectScreens);
 
-    const assets = (await Promise.all(projectScreens.map(async (screen) => getAssetData(screen, projectId, formats)))).flat();
+    const assets = (await Promise.all(projectScreens.map(async (screen) => getAssetData(screen, projectId, formats, densities)))).flat();
     updateMetadata.assets(assets);
+
+    const screensLayers = await Promise.all(projectScreens.map(async (screen) => getLayerData(screen, projectId)));
+    updateMetadata.layers(screensLayers);
 
     const assetsBar = new Progress("  Downloading project assets [:bar] :rate/bps :percent :etas", {
       complete: "=",
@@ -147,7 +267,7 @@ program
       await Promise.all(downloadAssetPromises);
     }
 
-    console.log(JSON.stringify(metadata));
+    saveMetadata(directory, metadata);
   });
 
 program.parse(process.argv);

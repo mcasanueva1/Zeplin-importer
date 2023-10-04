8141868257c8901bc3fa0ce3bb6ed8e2eaf0b67d
mcasanueva1 <116030641+mcasanueva1@users.noreply.github.com>
2023-10-04 15:59:24 -0300

    Update download-clm-project.js

--- download-clm-project.js
+++ download-clm-project.js
@@ -31,13 +31,22 @@ const getProjectProperties = async (projectId) => {
   return data;
 };
 
-const getProjectScreens = async (projectId, screenId, offset, limit) => {
-  const { data } = await zeplin.screens.getProjectScreens(projectId, { offset: offset, limit: limit });
+const getProjectScreens = async (projectId, screenId) => {
+  let screensCount = metadata.project.screensCount;
+  let pageLimit = 30;
+  let screensDownloaded = 0;
+  let screensData = [];
+
+  do {
+    const { data } = await zeplin.screens.getProjectScreens(projectId, { offset: screensDownloaded, limit: pageLimit });
+    screensData = screensData.concat(data);
+    screensDownloaded += data.length;
+  } while (screensDownloaded < screensCount - 1);
 
   if (screenId) {
-    return data.filter((screen) => screen.id === screenId);
+    return screensData.filter((screen) => screen.id === screenId);
   } else {
-    return data;
+    return screensData;
   }
 };
 
@@ -167,6 +176,7 @@ const mF = {
       template.name = screen.name;
       metadata.screens.data.push(template);
     });
+    delete metadata.screens.template;
   },
   layers: (layers) => {
     layers.flat().forEach((layer) => {
@@ -276,9 +286,6 @@ const mF = {
       }
     });
   },
-  cleanUp: () => {
-    delete metadata.screens.template;
-  },
 };
 
 const activityLog = {
@@ -306,53 +313,42 @@ program
   .action(async ({ projectId, screenId, metadataOnly, formats, densities }) => {
     mF.projectId(projectId);
 
-    //project properties
     const { name: projectName, numberOfScreens: screensCount } = await getProjectProperties(projectId);
     mF.projectName(projectName);
     mF.projectScreens(screensCount);
 
-    //output folde
+    const projectScreens = await getProjectScreens(projectId, screenId);
+    mF.screens(projectScreens);
+
+    const layers = await Promise.all(projectScreens.map(async (screen) => getLayersData(screen, projectId)));
+    mF.layers(layers);
+
+    const assets = await Promise.all(projectScreens.map(async (screen) => getAssetData(screen, projectId, formats, densities)));
+    mF.assets(assets);
+
+    mF.config();
+
+    const assetsBar = new Progress("  Downloading project assets [:bar] :rate/bps :percent :etas", {
+      complete: "=",
+      incomplete: " ",
+      width: 20,
+      total: assets.flat().length,
+    });
     let desktopPath = path.join(process.env.HOME, "Desktop");
     let directory = path.join(desktopPath, metadata.project.name.replaceAll("/", "-") + "__assets");
+
+    // Remove existing Output folder and create new one
     await fs.rm(directory, { recursive: true, force: true });
     await fs.mkdir(directory);
 
-    //screens pagination loop
-    let pageLimit = 30;
-    let screensDownloaded = 0;
-    do {
-      const screensBatch = await getProjectScreens(projectId, screenId, screensDownloaded, pageLimit);
-      mF.screens(screensBatch);
+    const limit = pLimit(20);
 
-      const layers = await Promise.all(screensBatch.map(async (screen) => getLayersData(screen, projectId)));
-      mF.layers(layers);
+    if (!metadataOnly) {
+      const downloadAssetPromises = assets.flat().map((asset) => limit(() => downloadAsset(asset, directory, assetsBar)));
 
-      const assets = await Promise.all(screensBatch.map(async (screen) => getAssetData(screen, projectId, formats, densities)));
-      mF.assets(assets);
+      await Promise.all(downloadAssetPromises);
+    }
 
-      //progress bar
-      const assetsBar = new Progress("  Downloading project assets [:bar] :rate/bps :percent :etas", {
-        complete: "=",
-        incomplete: " ",
-        width: 20,
-        total: assets.flat().length,
-      });
-
-      //download assets
-      if (!metadataOnly) {
-        const limit = pLimit(20);
-        const downloadAssetPromises = assets.flat().map((asset) => limit(() => downloadAsset(asset, directory, assetsBar)));
-
-        await Promise.all(downloadAssetPromises);
-      }
-
-      screensDownloaded += screensBatch.length;
-    } while (screensDownloaded < screensCount - 1);
-
-    //parse screens config
-    mF.config();
-
-    //save metadata and log
     mF.save(directory, metadata);
 
     activityLog.save(directory);
